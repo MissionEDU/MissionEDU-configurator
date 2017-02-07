ede8dad00f7f854bf1ea9057175c42211d0e1dc5
Daniel Maximilian Swoboda <i12032@student.htlwrn.ac.at>
2017-02-07 13:56:37 +0100

    Added functionality to connect
    Connected List now shows the connected clients, gives error message if
    already connect, lets connect to 127.0.0.1 on click

--- configurator.cpp
+++ configurator.cpp
@@ -1,5 +1,8 @@
 #include "configurator.h"
 #include "ui_configurator.h"
+#include <QMessageBox>
+using namespace nlohmann;
+using namespace std;
 
 Configurator::Configurator(QWidget *parent) :
     QMainWindow(parent),
@@ -8,6 +11,50 @@ Configurator::Configurator(QWidget *parent) :
     ui->setupUi(this);
 }
 
+void Configurator::on_addClientButton_clicked() {
+    try{
+        ConfiguratorHandler handle{};
+        Swockets* swocket = new Swockets(SwocketMode::ISCLIENT, &handle, "127.0.0.1");
+        handles.push_back(swocket);
+
+        QListWidgetItem* widgetItem = new QListWidgetItem();
+        QWidget* widget = new QWidget();
+        QLabel* labelIP;
+        QLabel* statusIcon;
+        labelIP = new QLabel("127.0.0.1", this);
+
+        QVBoxLayout* labelIPLayout = new QVBoxLayout();
+        labelIPLayout->addWidget(labelIP);
+        labelIPLayout->setMargin( 0 );
+        labelIPLayout->setSpacing( 0 );
+
+        // Create info pane
+        statusIcon = new QLabel(this);
+        statusIcon->setFixedSize(10, 10);
+        statusIcon->setStyleSheet("border:0px solid grey; border-radius: 5px;background-color: #00ff00;");
+
+        QHBoxLayout* generalLayout = new QHBoxLayout(this);
+        generalLayout->addWidget(statusIcon);
+        generalLayout->addLayout(labelIPLayout, 1);
+        generalLayout->setMargin( 5);
+        generalLayout->setSpacing( 4 );
+
+
+        widget->setLayout(generalLayout);
+        widget->setStyleSheet("background-color: #575757;");
+
+        widgetItem->setSizeHint(QSize(widgetItem->sizeHint().width(), 30));
+        ui->connectedList->addItem(widgetItem);
+        ui->connectedList->setItemWidget(widgetItem, widget);
+    } catch(...) {
+        QMessageBox msgBox;
+        msgBox.setText("You are already connected to this controller");
+        msgBox.exec();
+    }
+
+
+
+}
 Configurator::~Configurator()
 {
     delete ui;
